952fbd3599b2da3cd8343506b69d19c94ba52c55
milon2267 <gdazad2267@gmail.com>
2022-04-25 16:42:46 +0600

    product page update

--- backend/productData.js
+++ backend/productData.js
@@ -4,80 +4,88 @@ const productData = [
         img:"assets/images/product.png",
         rating:5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"30"
+        price:"30",
+        brand:"ZARA"
     },
     {
         id:"2",
         img:"assets/images/product1.png",
         rating:4.5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"25"
+        price:"25",
+        brand:"ZARA"
     },
     {
         id:"3",
         img:"assets/images/product2.png",
         rating:3.5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"20"
+        price:"20",
+        brand:"ZARA"
     },
     {
         id:"4",
         img:"assets/images/product3.png",
         rating:5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"35"
+        price:"35",
+        brand:"ZARA"
     },
     {
         id:"5",
         img:"assets/images/product4.png",
-        rating:5,
+        rating:3.5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"40"
+        price:"40",
+        brand:"ZARA"
     },
     {
         id:"6",
         img:"assets/images/product5.png",
-        rating:5,
+        rating:4,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"30"
+        price:"30",
+        brand:"ZARA"
     },
     {
         id:"7",
         img:"assets/images/product6.png",
         rating:5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"25"
+        price:"25",
+        brand:"ZARA"
     },
     {
         id:"8",
         img:"assets/images/product7.png",
-        rating:5,
+        rating:3.5,
         title:"Quilted gilet with hood",
-        color:"",
-        size:"",
+        colors:["622060","1473A9","FBDADA","519D04"],
+        sizes:["XS","S","M"],
         cart:"",
-        price:"40"
+        price:"40",
+        brand:"ZARA"
     },
 ]
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Panel,Col } from 'rsuite';
-import ReactStars from "react-rating-stars-component";
+import { BsStarFill,BsStarHalf,BsStar } from 'react-icons/bs';
+import { HiOutlineShoppingBag } from 'react-icons/hi';
 
 
 const Product = () => {
@@ -19,27 +20,42 @@ const Product = () => {
     <>
         <Panel >
             {product.map(item=>(
-                <Col xs={6}>
-                    <img src={item.img} width="100%" alt="" />
+                <Col xs={6} className='productcard'>
+                    <img src={item.img} width="100%" alt="img" />
+                    <div className='productbox'>
+                        <div className='producticon'>
+                            {item.rating >= 1 ? <BsStarFill className='staricon'/>:item.rating >= .5 ? <BsStarHalf className='staricon'/>:<BsStar className='staricon'/>}
+                            {item.rating >= 2 ? <BsStarFill className='staricon'/>:item.rating >= 1.5 ? <BsStarHalf className='staricon'/>:<BsStar className='staricon'/>}
+                            {item.rating >= 3 ? <BsStarFill className='staricon'/>:item.rating >= 2.5 ? <BsStarHalf className='staricon'/>:<BsStar className='staricon'/>}
+                            {item.rating >= 4 ? <BsStarFill className='staricon'/>:item.rating >= 3.5 ? <BsStarHalf className='staricon'/>:<BsStar className='staricon'/>}
+                            {item.rating >= 5 ? <BsStarFill className='staricon'/>:item.rating >= 4.5 ? <BsStarHalf className='staricon'/>:<BsStar className='staricon'/>}
+                        </div>
+                        <div className='brand'>
+                            <p>{item.brand}</p>
+                        </div>
+                    </div>
                     <Panel>
-                        <ReactStars
-                            count={5}
-                            size={24}
-                            isHalf={true}
-                            activeColor="#ffd700"
-                            value={item.rating}
-                            edit={false}
-                        />
+                        <p className='productTitle'>{item.title}</p>
                         <p>
-                            <small>
-                                {item.title}
-                            </small>
+                            <div className='productbox'>
+                                <div className='producticon'>
+                                    {item.colors.map(items=>(
+                                        <span className='productColors' style={{background: `#${items}`}}></span>
+                                    ))}
+                                </div>
+                                <div className='productsize'>
+                                    {item.sizes.map(items=>(
+                                        <span className='productSizes'>{items}</span>
+                                    ))}
+                                </div>
+                            </div>
                         </p>
-                        <p>
-                            <small>
-                                {item.color}
-                            </small>
-                        </p>
+                        <span className='productcart'>
+                            <HiOutlineShoppingBag className='carticon'/>
+                        </span>
+                        <span className='price'>
+                            $ {item.price}
+                        </span>
                     </Panel>
                 </Col>
             ))}
